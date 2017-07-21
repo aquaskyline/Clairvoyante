56f698f755205e8451154f0bfe9865e2f2f1dea5
aquaskyline <aquaskyline@gmail.com>
2017-07-21 11:59:58 -0400

    July 21 1140

--- clairvoyante/param.py
+++ clairvoyante/param.py
@@ -3,5 +3,5 @@ matrixNum = 4    #1: ref, 2: ins, 3: del, 4: snp
 batchSize = 500
 minDepth = 4
 dropoutRate = 0.5
-initialLearningRate = 0.01
-learningRateDecay = 0.1
+initialLearningRate = 0.005
+learningRateDecay = 0.125
