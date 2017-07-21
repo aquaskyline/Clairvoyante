4497f82c95b4dcbfaeb89a519bcd5eddf15db184
aquaskyline <aquaskyline@gmail.com>
2017-07-21 00:31:57 -0400

    July 21 commit before sleep

--- clairvoyante/param.py
+++ clairvoyante/param.py
@@ -2,6 +2,6 @@ flankingBaseNum = 7
 matrixNum = 4    #1: ref, 2: ins, 3: del, 4: snp
 batchSize = 500
 minDepth = 4
-dropoutRate = 0.3
+dropoutRate = 0.5
 initialLearningRate = 0.01
 learningRateDecay = 0.1
