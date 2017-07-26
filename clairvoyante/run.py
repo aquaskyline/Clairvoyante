622777ea1df2c04cb1bbf99a10499728c6f1fd53
aquaskyline <rluo5@jhu.edu@schatz-bigmem.cm.cluster>
2017-07-26 18:15:07 -0400

    Wed Jul 26 18:15:07 EDT 2017

--- clairvoyante/run.py
+++ clairvoyante/run.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import param
 
-#import clairvoyante as cv
+# import clairvoyante
 print >> sys.stderr, "Importing Clairvoyante ..."
 import utils as utils
 import clairvoyante as cv
@@ -11,7 +11,7 @@ import clairvoyante as cv
 # load the generate alignment tensors
 # use only variants overlapping with the high confident regions defined in `CHROM21_v.3.3.2_highconf_noinconsistent.bed`
 
-print >> sys.stderr, "Loading training dataset ..."
+print >> sys.stderr, "Loading the training dataset ..."
 XArray, YArray, posArray = \
 utils.GetTrainingArray("../training/tensor_pi_mul",
                        "../training/var_mul",
@@ -30,14 +30,14 @@ m.init()
 logsPath = "../training/logs"
 summaryWriter = m.summaryFileWriter(logsPath)
 
-# training and save the parameters, we train on all variant sites and validate on the last 15% variant sites
+# training and save the parameters, we train on all variant sites and validate on the last 10% variant sites
 print >> sys.stderr, "Start training ..."
 trainingStart = time.time()
 trainBatchSize = param.trainBatchSize
 validationLosts = []
 numValItems = int(len(XArray) * 0.1 + 0.499)
 
-c = 0; maxLearningRateSwitch = 5
+c = 0; maxLearningRateSwitch = 10
 epochStart = time.time()
 for i in range(1, 1 + int(param.maxEpoch * len(XArray) / trainBatchSize + 0.499)):
     XBatch, YBatch = utils.GetBatch(XArray, YArray, size=trainBatchSize)
@@ -47,7 +47,7 @@ for i in range(1, 1 + int(param.maxEpoch * len(XArray) / trainBatchSize + 0.499)
         validationLost = m.getLoss( XArray[-numValItems:-1], YArray[-numValItems:-1] )
         print >> sys.stderr, i, "Training lost:", loss/trainBatchSize, "Validation lost: ", validationLost/numValItems
         validationLosts.append( (validationLost, i) )
-        print >> sys.stderr, "Epoch time elapsed: %.2f s" % (time.time() - epochStart)
+        print >> sys.stderr, "Time elapsed for 1 epoch: %.2f s" % (time.time() - epochStart)
         flag = 0
         if c >= 4:
           if validationLosts[-4][0] - validationLosts[-3][0] > 0:
@@ -70,7 +70,6 @@ for i in range(1, 1 + int(param.maxEpoch * len(XArray) / trainBatchSize + 0.499)
         c += 1
         epochStart = time.time()
 
-
 print >> sys.stderr, "Training time elapsed: %.2f s" % (time.time() - trainingStart)
 
 # pick the parameter set of the smallest validation loss
@@ -79,7 +78,7 @@ i = validationLosts[0][1]
 print >> sys.stderr, "Best validation lost at batch: %d" % i
 #m.restoreParameters('../training/parameters/cv.params-%05d' % i)
 
-print >> sys.stderr, "Predicting on training ..."
+print >> sys.stderr, "Testing on the training dataset ..."
 predictStart = time.time()
 predictBatchSize = param.predictBatchSize
 bases, ts = m.predict(XArray[0:predictBatchSize])
@@ -89,7 +88,7 @@ for i in range(predictBatchSize, len(XArray), predictBatchSize):
     ts = np.append(ts, t, 0)
 print >> sys.stderr, "Prediciton time elapsed: %.2f s" % (time.time() - predictStart)
 
-print >> sys.stderr, "Model evaluation on training data:"
+print >> sys.stderr, "Model evaluation on the training dataset:"
 ed = np.zeros( (5,5), dtype=np.int )
 for predictV, annotateV in zip(ts, YArray[:,4:]):
     ed[np.argmax(annotateV)][np.argmax(predictV)] += 1
@@ -100,13 +99,13 @@ for i in range(5):
           print >> sys.stderr, ed[i][j],"\t",
       print >> sys.stderr
 
-print >> sys.stderr, "Loading chr22 dataset ..."
+print >> sys.stderr, "Loading the chr22 dataset ..."
 XArray2, YArray2, posArray2 = \
 utils.GetTrainingArray("../training/tensor_pi_chr22",
                        "../training/var_chr22",
                        "../testingData/chr22/CHROM22_v.3.3.2_highconf_noinconsistent.bed")
 
-print >> sys.stderr, "Predicting on chr22 dataset ..."
+print >> sys.stderr, "Testing on the chr22 dataset ..."
 predictStart = time.time()
 predictBatchSize = param.predictBatchSize
 bases, ts = m.predict(XArray2[0:predictBatchSize])
@@ -116,7 +115,7 @@ for i in range(predictBatchSize, len(XArray2), predictBatchSize):
     ts = np.append(ts, t, 0)
 print >> sys.stderr, "Prediciton time elapsed: %.2f s" % (time.time() - predictStart)
 
-print >> sys.stderr, "Model evaluation on chr22 dataset:"
+print >> sys.stderr, "Model evaluation on the chr22 dataset:"
 ed = np.zeros( (5,5), dtype=np.int )
 for predictV, annotateV in zip(ts, YArray2[:,4:]):
     ed[np.argmax(annotateV)][np.argmax(predictV)] += 1
